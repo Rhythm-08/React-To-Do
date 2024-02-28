a8b31e5bd99840bd16a7404be52fc836c536a5b6
Rhythm-08 <rhythmsharma808@gmail.com>
2024-02-28 22:35:02 +0530

    feat: added the list and useState to store the toDo list data and display on the screen

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,19 +1,49 @@
 import './HomeComponent.css';
+import { ChangeEvent, useState } from 'react';
 
 const HomeComponent = () => {
+
+    const [task, setTask] = useState('');
+    const [toDoList,setToDoList] = useState<string[]>([]);
+
+    const handleSubmission = (event:any) => {
+        event.preventDefault();
+        if(task.length){
+            setToDoList((prevToDoList) => [...prevToDoList, task]);
+            setTask(''); 
+        }
+        console.log(toDoList);
+        
+    }
+
     return (
-        <form action="">
-            <div className="home-page">
-                <div className="input-box">
-                    <div>
-                        <input type="text" aria-label="add title" className="input-id" placeholder='Enter Task'/>
-                    </div>
-                    <div>
-                        <button className="add-btn" type='submit'>Add</button> 
+        <>
+            <form action=""  onSubmit={ handleSubmission}>
+                <div className="home-page">
+                    <div className="input-box">
+                        <div>
+                            <input type="text" aria-label="add title" className="input-id" placeholder='Enter Task' value = {task}
+                            onChange = {(e: ChangeEvent<HTMLInputElement>) => setTask(e.target.value)} />
+                        </div>
+                        <div>
+                            <button className="add-btn" type='submit'>Add</button> 
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+            {
+                toDoList.map((name, index) => (
+                    <div key={index} className='list'>
+                        <div className='list-block'>
+                        <button className='task-complete'>Mark Complete</button>
+
+                            <span className='task-name'>{name}</span>
+                            <button className='task-delete'>Delete</button>
+                        </div>
+                    </div>
+                ))
+            }
+        </>
      );
 }
  
