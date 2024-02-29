18cc0dce2cc99298e1182ba05103518b579e34a4
Rhythm-08 <rhythmsharma808@gmail.com>
2024-02-29 22:47:12 +0530

    feat: implemented GET and POST method to store data

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,32 +1,83 @@
 import './HomeComponent.css';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 const HomeComponent = () => {
 
     const [task, setTask] = useState('');
-    const [toDoList,setToDoList] = useState<string[]>([]);
+    const [toDoList, setToDoList] = useState<string[]>([]);
 
-    const handleSubmission = (event:any) => {
+    const handleSubmission = async (event: any) => {
         event.preventDefault();
-        if(task.length){
+        await postData(task);
+        if (task.length) {
             setToDoList((prevToDoList) => [...prevToDoList, task]);
-            setTask(''); 
+            setTask('');
         }
         console.log(toDoList);
-        
     }
+
+    const postData = async (data: string) => {
+        fetch('http://localhost:3000/todoList', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Post request successful: ', data);
+            })
+            .catch(error => {
+                console.error('Error during post request: ', error);
+
+            });
+    };
+
+    const getToDoList = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/todoList', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            console.log('Requested data is here:', data);
+
+            if (data && data.length > 0) {
+                setToDoList([...data.map((list: any) => list?.data)]);
+            }
+        } catch (error) {
+            console.error('Error during get request:', error);
+        }
+    };
+
+    useEffect(() => {
+        getToDoList();
+    }, []);
 
     return (
         <>
-            <form action=""  onSubmit={ handleSubmission}>
+            <form action="" onSubmit={handleSubmission}>
                 <div className="home-page">
                     <div className="input-box">
                         <div>
-                            <input type="text" aria-label="add title" className="input-id" placeholder='Enter Task' value = {task}
-                            onChange = {(e: ChangeEvent<HTMLInputElement>) => setTask(e.target.value)} />
+                            <input type="text" aria-label="add title" className="input-id" placeholder='Enter Task' value={task}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => setTask(e.target.value)} />
                         </div>
                         <div>
-                            <button className="add-btn" type='submit'>Add</button> 
+                            <button className="add-btn" type='submit'>Add</button>
                         </div>
                     </div>
                 </div>
@@ -35,7 +86,7 @@ const HomeComponent = () => {
                 toDoList.map((name, index) => (
                     <div key={index} className='list'>
                         <div className='list-block'>
-                        <button className='task-complete'>Mark Complete</button>
+                            <button className='task-complete'>Mark Complete</button>
 
                             <span className='task-name'>{name}</span>
                             <button className='task-delete'>Delete</button>
@@ -44,7 +95,7 @@ const HomeComponent = () => {
                 ))
             }
         </>
-     );
+    );
 }
- 
+
 export default HomeComponent;
